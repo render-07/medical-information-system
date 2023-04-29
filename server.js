4cc010fc9b614cb9afe8bf1acfa6f206f316522d
Redner Ivan Cabra <rednerivanpadasaycabra.stack@gmail.com>
2023-04-29 19:49:41 +0800

    update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Bring in config
 const config = require("config");
-//const path = require("path");
+const path = require("path");
 
 // Body-parser middleware
 // Body-parser allow us to take request and get the data from the body
@@ -57,14 +57,14 @@ mongoose
   .catch((err) => console.log(err));
 
 // Serve static assets if in production
-// if (process.env.NODE_ENV === "production") {
-//   // Set static folder
-//   app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Use routes
 // Anything the goes to api/items, should refer to items variable
