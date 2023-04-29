a6c0b47b85cbbe1d3c3f2e7b7f7b33982537234d
Redner Ivan Cabra <rednerivanpadasaycabra.stack@gmail.com>
2023-04-29 13:26:56 +0800

    update nodemailer credentials

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -11,7 +11,7 @@ const sendEmail = async (email, subject, payload, template) => {
       port: 465,
       auth: {
         user: "rtumistorage@gmail.com",
-        pass: "rtumistorageadmin", // naturally, replace both with your real credentials or an application-specific password
+        pass: "egxfyuyfieozahcc", // rtumistorageadmin = naturally, replace both with your real credentials or an application-specific password
       },
     });
 
